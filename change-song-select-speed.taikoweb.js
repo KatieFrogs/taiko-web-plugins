50a47b9579eb96558c62442f3665e5d02bb44cb0
KatieFrogs <23621460+KatieFrogs@users.noreply.github.com>
2022-02-16 18:28:59 +0300

    Add Change Song Select Speed plugin

---
+++ change-song-select-speed.taikoweb.js
@@ -0,0 +1,20 @@
+export default class Plugin extends Patch{
+	name = "Change Song Select Speed"
+	version = "22.02.16"
+	description = "Changes the song selection scroll speed"
+	author = "Katie Frogs"
+	
+	load(){
+		var speed = 0.5
+		
+		this.addEdits(
+			new EditFunction(SongSelect.prototype, "init").load(str => {
+				return plugins.insertAfter(str, 'speed: 400', `/ ${speed}`)
+			})
+		)
+	}
+}
+
+if(typeof GM_info === "object"){
+	addEventListener("ready", () => plugins.add(Plugin)?.start())
+}
