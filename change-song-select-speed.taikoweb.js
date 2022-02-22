6aefeff8ac7b9f4cb84a49dea33ad88cbd9a457b
KatieFrogs <23621460+KatieFrogs@users.noreply.github.com>
2022-02-22 16:56:48 +0300

    Add settings and limit bpm display

--- change-song-select-speed.taikoweb.js
+++ change-song-select-speed.taikoweb.js
@@ -1,16 +1,50 @@
 export default class Plugin extends Patch{
 	name = "Change Song Select Speed"
-	version = "22.02.16"
+	version = "22.02.22"
 	description = "Changes the song selection scroll speed"
 	author = "Katie Frogs"
 	
+	selectRate = 0.5
+	
+	strings = {
+		selectRate: {
+			name: "Song Select Speed",
+			name_lang: {},
+			description: null,
+			description_lang: {},
+			format: "%sx"
+		}
+	}
+	
 	load(){
-		var speed = 0.5
-		
 		this.addEdits(
 			new EditFunction(SongSelect.prototype, "init").load(str => {
-				return plugins.insertAfter(str, 'speed: 400', `/ ${speed}`)
-			})
+				return plugins.insertAfter(str, 'speed: 400', `/ this.getSelectRate()`)
+			}),
+			new EditValue(SongSelect.prototype, "getSelectRate").load(() => this.getSelectRate.bind(this))
 		)
+	}
+	getSelectRate(){
+		return this.selectRate
+	}
+	settings(){
+		var str = this.strings.selectRate
+		return [{
+			name: str.name,
+			name_lang: str.name_lang,
+			description: str.description,
+			description_lang: str.description_lang,
+			format: str.format,
+			format_lang: str.format_lang,
+			type: "number",
+			min: 0.25,
+			fixedPoint: 2,
+			step: 25,
+			default: this.selectRate,
+			getItem: () => this.selectRate,
+			setItem: value => {
+				this.selectRate = value
+			}
+		}]
 	}
 }
--- show-bpm.taikoweb.js
+++ show-bpm.taikoweb.js
@@ -3,7 +3,7 @@ export default class Plugin extends Patch{
 	name_lang = {
 		ja: "BPMを表示する"
 	}
-	version = "22.02.11"
+	version = "22.02.22"
 	description = "Displays the current BPM in game"
 	author = "Katie Frogs"
 	
@@ -13,7 +13,7 @@ export default class Plugin extends Patch{
 				return plugins.insertBefore(str, `
 				this.draw.layeredText({
 					ctx: ctx,
-					text: "BPM: " + (1000 / this.beatInterval * 60).toString(),
+					text: "BPM: " + (Math.floor(1000 / this.beatInterval * 60 * 1000) / 1000).toString(),
 					fontSize: 30,
 					fontFamily: this.font,
 					x: 10,
