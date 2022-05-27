9b5a2fc287194c0adf07c467a7d954cc163facb6
김성진 <mirusu400@naver.com>
2022-05-27 21:38:58 +0900

    Fix: Search in english subtitle

--- song-select/search-in-english.taikoweb.js
+++ song-select/search-in-english.taikoweb.js
@@ -3,7 +3,7 @@ export default class Plugin extends Patch {
     version = "22.05.27"
     description = "Search by english title name."
     author = "mirusu400"
-
+    
     strings = {
 
     }
@@ -14,23 +14,24 @@ export default class Plugin extends Patch {
                 return plugins.insertAfter(str, `song.subtitlePrepared = subtitle ? fuzzysort.prepare(this.search.normalizeString(subtitle)) : null`, `
 					var enTitle = song.title_lang.en
 			        song.enTitlePrepared = enTitle ? fuzzysort.prepare(this.search.normalizeString(enTitle)) : null
+                    var enSubTitle = song.subtitle_lang.en
+                    song.enSubTitlePrepared = enSubTitle ? fuzzysort.prepare(this.search.normalizeString(enSubTitle)) : null
 				`)
             }),
             new EditFunction(Search.prototype, "perform").load(str => {
                 return plugins.strReplace(str, `["titlePrepared", "subtitlePrepared"]`,
-                `["titlePrepared", "subtitlePrepared", "enTitlePrepared"]`
+                    `["titlePrepared", "subtitlePrepared", "enTitlePrepared", "enSubTitlePrepared"]`
                 )
             }),
             new EditFunction(Search.prototype, "perform").load(str => {
                 return plugins.strReplace(str, `return a[1] ? score1 : -Infinity`,
-                    `return a[1] ? score1 : a[2] ? -9999 : -Infinity`
+                    `return a[1] ? score1 : a[2] ? -9999 : a[3] ? -19999 : -Infinity`
                 )
-            }),
-
+            })
         )
     }
 
     settings() {
-        return []
+       return []
     }
 }
