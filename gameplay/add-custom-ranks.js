7e30d937baf5cd6b09f51c8f1978dff5016e7292
DJTOMATO <48269777+DJTOMATO@users.noreply.github.com>
2022-06-10 22:45:13 -0400

    Update add-custom-ranks.js
    Final version, all screensizes working
    and ``EditFunction`` reduced as much as possible
    
    Song Select: OK
    Gameplay:
     - Mobile: OK
     - Desktop: OK
    Score Results: OK

--- gameplay/add-custom-ranks.js
+++ gameplay/add-custom-ranks.js
@@ -17,24 +17,6 @@ export default class Plugin extends Patch {
 				case 'bol':
 		rank = "Charter";
 		break;
-				case 'NotTang':
-		rank = "Rank 1";
-		break;
-				case 'Meowgister':
-		rank = "Charter";
-		break;
-				case 'Dormin':
-		rank = "Spammer";
-		break;
-				case 'colinmcguire':
-		rank = "Makima's Dog";
-		break;
-				case 'sakurada0291':
-		rank = "Summertime Melodies";
-		break;
-				case 'Thusuzzee':
-		rank = "nekos.best";
-		break;
 				case '5':
 		rank = "Charter";
 		break;
@@ -57,11 +39,13 @@ export default class Plugin extends Patch {
 	load() {
 		this.log("load")
 		this.addEdits(
+			// Song Select Edits
 			// Adds Rank to Song Select
 			new EditFunction(SongSelect.prototype, "redraw").load(str => {
 				str = plugins.insertBefore(str, this.method, `this.nameplateCache.get({`)
 				return str
 			}),
+			// Score Result Edits
 			// Prepares Custom Rank for Score Result
 			new EditFunction(Scoresheet.prototype, "redraw").load(str => {
 				str = plugins.insertBefore(str, `var rank = 'test'
@@ -76,58 +60,34 @@ export default class Plugin extends Patch {
 				return str
 			}),
 
-			//////// Below needs refactoring/rewrite.
-
-			// Defines rank for gameplay screen
+			// Gameplay Edits
 			new EditFunction(View.prototype, "refresh").load(str => {
+				// Defines rank for gameplay screen
 				str = plugins.insertBefore(str, `var rank = "test"
 					`, `var winW = innerWidth`)
-				return str
-			}),
-
-			new EditFunction(View.prototype, "refresh").load(str => {
-				str = plugins.insertBefore(str, this.method, `var winH = lastHeight`)
-				return str
-			}),
-			// Inserts rank value into gameplay (Mobile)
-			new EditFunction(View.prototype, "refresh").load(str => {
+				// Inserts rank value into gameplay (Mobile)
 				str = plugins.insertBefore(str, this.method, `				this.draw.nameplate({
 					ctx: ctx,
 					x: 3,
 					y: 3,
 					scale: 0.8,`)
-				return str
-			}),
-			// Adds rank to gameplay screen (Mobile)
-			new EditFunction(View.prototype, "refresh").load(str => {
+				// Adds rank to gameplay screen (Mobile)
 				str = plugins.insertBefore(str, `rank: rank,
 
 					`, `scale: 0.8,`)
-				return str
-			}),
-			// Inserts rank value into gameplay (PC)
-			new EditFunction(View.prototype, "refresh").load(str => {
+				// Inserts rank value into gameplay (PC)
 				str = plugins.insertBefore(str, this.method, `				this.draw.nameplate({
 					ctx: ctx,
 					x: 3,
 					y: 3,
 					name: name`)
-				return str
-			}),
-			// Adds rank to gameplay screen (PC)
-			new EditFunction(View.prototype, "refresh").load(str => {
+				// Adds rank to gameplay screen (PC)
 				str = plugins.insertBefore(str, `rank: rank,
 
 					`, `					y: 3,
 					name: name,`)
 				return str
 			}),
-
-
-
-
-
-
 		)
 	}
 	start() {
